7d9cf79aa46dea90666a574736b00378d9c85626
Nguyen Hoang Phuc <phucnguyenhoang1985@gmail.com>
2016-06-10 08:32:28 +0900

    new layout

--- app/components/common/item.js
+++ app/components/common/item.js
@@ -1,19 +1,28 @@
 import React, { PropTypes } from 'react'
+import { numberWithCommas } from 'utils/common'
 
 const Item = (props) => {
   const { item } = props
 
+
+
   return (
     <div className="item" >
-      <div className="item__img">
+      <div className="item__img-wrap">
         <img src={item.mediumImageUrls[0].imageUrl} />
       </div>
-
-
-
-
+      <div className="item__name">
+        <span>{item.itemName}</span>
+      </div>
+      <div className="item__price">
+        <span>{`${numberWithCommas(item.itemPrice)} 円`}</span>
+      </div>
     </div>
   )
+}
+
+Item.propTypes = {
+  item: PropTypes.object
 }
 
 export default Item

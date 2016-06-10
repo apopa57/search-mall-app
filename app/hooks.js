74a0652eef9bf3b63e4433fbdd3ddd38a9eac8fb
Nguyen Phuc <phuc.nguyen@mail.rakuten.com>
2016-06-10 12:53:31 +0900

    added new

---
+++ app/hooks.js
@@ -0,0 +1,10 @@
+import { bindActionCreators } from 'redux';
+import { fetchGenresIfNeeded } from 'actions/genre';
+
+export function bootstrap({ dispatch }) {
+  const fetchGenres = bindActionCreators(fetchGenresIfNeeded, dispatch)
+
+  return () => {
+    fetchGenres(0)
+  };
+}
